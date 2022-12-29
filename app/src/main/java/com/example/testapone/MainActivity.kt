7a77202f42a590a39619a596254f299ba641e4a6
Khondakar Afridi <khondakarafridi35@gmail.com>
2022-12-29 21:15:07 +0600

    ClapAppDay 3 init

--- app/src/main/java/com/example/testapone/MainActivity.kt
+++ app/src/main/java/com/example/testapone/MainActivity.kt
@@ -13,60 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var weightTF = findViewById<EditText>(R.id.weightTF)
-        var heightTF = findViewById<EditText>(R.id.heightTF)
-        var calculateBtn = findViewById<Button>(R.id.calculateBTN)
-
-        calculateBtn.setOnClickListener(){
-            if(validateInput(weightTF.text, heightTF.text)){
-                var weight = weightTF.text.toString()
-                var height = heightTF.text.toString()
-                var bmi = weight.toFloat()/(((height.toFloat())*(height.toFloat()))/10000)
-                bmi = String.format("%.2f", bmi).toFloat()
-                displayResult(bmi)
-            }
-        }
     }
 
-    private fun validateInput(weight: Editable, height: Editable): Boolean{
-        return when{
-            weight.isNullOrEmpty()-> {
-                Toast.makeText(this@MainActivity, "Weight is empty", Toast.LENGTH_LONG).show()
-                return false
-            }
-            height.isNullOrEmpty()->{
-                Toast.makeText(this@MainActivity, "Height is empty", Toast.LENGTH_LONG).show()
-                return false
-            }
-            else -> {
-                return true
-            }
-        }
-    }
-
-    private fun displayResult(bmi: Float){
-        var bmiResultTV = findViewById<TextView>(R.id.bmiResultTV)
-        var bmiResultCommentTV = findViewById<TextView>(R.id.bmiResultCommentTV)
-
-        bmiResultTV.setText(bmi.toString())
-        var comment: String = ""
-        when{
-            bmi<18 ->{
-                comment="Underweight"
-            }
-            bmi in 19.00..24.00->{
-                comment="Healthy"
-            }
-            bmi in 24.00..29.00->{
-                comment="Overweight"
-            }
-            bmi>29.00->{
-                comment="Obese"
-            }
-        }
-        bmiResultCommentTV.text=comment
-    }
 }
 
 
