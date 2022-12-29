9a7fafcdc98efc42b422747a50337f011cb3b35e
Khondakar Afridi <khondakarafridi35@gmail.com>
2022-12-29 20:42:04 +0600

    Day 3 BMI App

--- app/src/main/java/com/example/testapone/MainActivity.kt
+++ app/src/main/java/com/example/testapone/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var weightTF = findViewById<EditText>(R.id.weightTF)
+        var heightTF = findViewById<EditText>(R.id.heightTF)
+        var calculateBtn = findViewById<Button>(R.id.calculateBTN)
+
+        calculateBtn.setOnClickListener(){
+            if(weightTF.text.isNotEmpty() || weightTF.text!=null || heightTF.text.isNotEmpty() || heightTF.text!=null){
+                var weight = weightTF.text.toString()
+                var height = heightTF.text.toString()
+                var bmi = weight.toFloat()/(((height.toFloat())*(height.toFloat()))/10000)
+                bmi = String.format("%.2f", bmi).toFloat()
+                displayResult(bmi)
+            }
+        }
+    }
+
+    private fun displayResult(bmi: Float){
+        var bmiResultTV = findViewById<TextView>(R.id.bmiResultTV)
+        var bmiResultCommentTV = findViewById<TextView>(R.id.bmiResultCommentTV)
+
+        bmiResultTV.setText(bmi.toString())
     }
 }
 
 
-//Done till 3:26:00
+//Done till 4:26:00
 //Link https://www.youtube.com/watch?v=BCSlZIUj18Y&t=44s&ab_channel=AppDevNotes
