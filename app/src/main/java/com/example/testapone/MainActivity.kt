c2d56e3e9b2a5ed6b43835cafcb6cfce11a1c109
Khondakar Afridi <khondakarafridi35@gmail.com>
2022-12-29 11:39:00 +0600

    BmiCal app init

--- app/src/main/java/com/example/testapone/MainActivity.kt
+++ app/src/main/java/com/example/testapone/MainActivity.kt
@@ -8,46 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var userNameTF: EditText
-    private lateinit var numberTF: EditText
-    private lateinit var saveBTN: Button
-
-    private lateinit var sharedPref:SharedPreferences
-    private lateinit var editor:SharedPreferences.Editor
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        userNameTF = findViewById<EditText>(R.id.userNameTF)
-        numberTF = findViewById<EditText>(R.id.numberTF)
-        saveBTN = findViewById<Button>(R.id.saveBTN)
-
-        sharedPref = getSharedPreferences("my_sharedPref", MODE_PRIVATE,)
-        editor=sharedPref.edit()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        val name = userNameTF.text.toString()
-        val number = numberTF.text.toString().toInt()
-        editor.apply{
-            putString("sf_name", name)
-            putInt("sf_number", number)
-            commit()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val name = sharedPref.getString("sf_name", null)
-        val number = sharedPref.getInt("sf_number", 0)
-
-        userNameTF.setText(name)
-        if(number!=0){
-            numberTF.setText(number.toString())
-        }else{
-            numberTF.setText("")
-        }
     }
 }
 
